cd6cca25c639388d7ac43b8e8ad11a9d2b923137
nainoNa <lanyu93@icloud.com>
2020-03-11 23:47:01 +0800

    add the first one
    添加第一个工具 -- 获取文件夹的目录结构
    并添加 README 的介绍

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Little-Tools
+
 Some simple tools.
+
+## 1. 获取文件夹的目录结构 getFileList.py
+
++ 操作系统 macOS (推测可用于 Linux)
++ 使用 [Typora](https://www.typora.io/) && Markdown 
+
+效果如图
+
+![IMG_6512](/Users/apple/Downloads/IMG_6512.JPEG)
+
+注：
+
++ `[TOC]` 生成 md 文件的目录
++ 小标题的不同层级表示了目录的不同层级
++ 文件使用 `[名字](绝对地址)` 的方式生成超链接，点击可打开。
++ 排除了 mac 系统生成的 `.DS_Store` 文件
+
+
+
+**目的** ： 整理文件较多的目录
+
+**使用方式**：
+
+```shell
+$ python3 "该工具的 .py 文件的绝对地址"
+```
+
+
+
+
+
+**TODO：**
+
+1. 修改地址需要改变文件中  `getContent('...')` 的参数，可以改成命令行输入地址。
+2. 配合 Automator 使用定时更新
+
+
+
---
+++ src/getFileList.py
@@ -0,0 +1,24 @@
+import os
+
+def getContent(filepath):
+    path_pair = filepath.rsplit('/',maxsplit=1)
+    with open(path_pair[0] + "/Cont_" + path_pair[1]+ ".md", mode='w') as op: 
+        print("[TOC]", file=op)
+        listdir(filepath,output=op)
+
+    
+def listdir(path, deep=0,output="/Users/apple/PycharmProjects/test/getFL_buf.txt"):
+    print('#' * (deep + 1) + os.path.split(path)[1],file=output)
+    dir_list = sorted(os.listdir(path))
+    deep += 1
+    for item in dir_list:
+        item_path = path + '/' + item
+        if os.path.isdir(item_path):
+            listdir(item_path, deep,output)
+        elif '.DS_Store' not in item:
+            print(' ' * deep + '+ [' + item + '](' + item_path + ')', file=output)
+
+      
+getContent('/Users/apple/Documents/Books/test')
+
+
